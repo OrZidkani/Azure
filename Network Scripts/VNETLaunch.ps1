6ef71f8b47fa6401da1a1bf730591ff1b54d3962
Or Zidkani <or6680@gmail.com>
2021-08-31 19:59:52 +0200

    Update VNETLaunch.ps1

--- Network Scripts/VNETLaunch.ps1
+++ Network Scripts/VNETLaunch.ps1
@@ -1,3 +1,5 @@
 $vnetonboarding = Invoke-WebRequest https://raw.githubusercontent.com/OrZidkani/Azure/main/Network%20Scripts/New%20VNET%20onboarding
 Invoke-Expression $($vnetonboarding.Content)
 Remove-Variable -Name vnetonboarding;
+
+

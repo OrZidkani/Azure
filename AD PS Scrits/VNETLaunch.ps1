ce15333ef0a2bb298a6322963064d6497375f856
Or Zidkani <or6680@gmail.com>
2021-08-31 19:03:54 +0200

    Create VNETLaunch.ps1

---
+++ AD PS Scrits/VNETLaunch.ps1
@@ -0,0 +1,3 @@
+$vnetonboarding = Invoke-WebRequest https://raw.githubusercontent.com/OrZidkani/Azure/main/Network%20Scripts/New%20VNET%20onboarding
+Invoke-Expression $($vnetonboarding.Content)
+Remove-Variable -Name vnetonboarding;
